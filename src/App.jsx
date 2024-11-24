fc8d7a3283c2b51e67af92e09d388f7fe4591ba6
RahulGhosh <rahul.chandra@bracits.com>
2024-11-24 17:41:21 +0600

    fix ui

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     // API call is proxied through Vite (no need to include the full backend URL)
-    fetch(`${import.meta.env.VITE_URL_VERSION}/posts`)
+    fetch(`${import.meta.env.VITE_URL_NAME}/posts`)
       .then((response) => response.json())
       .then((data) => {
         if (data?.posts?.length) {
@@ -22,13 +22,13 @@ function App() {
   return (
     <div className="space-y-10">
       <div>
-        {/* <p className="read-the-docs text-2xl font-bold underline">
+        <p className="read-the-docs text-2xl font-bold underline">
           === Http-Proxy-Middleware Tutorial ====
-        </p> */}
+        </p>
         <p className="read-the-docs mt-2">
           Change{" "}
-          <span className="font-bold">
-            Prefix {import.meta.env.VITE_URL_VERSION}
+          <span className="font-bold text-sm">
+            VITE_URL_NAME = {import.meta.env.VITE_URL_NAME}
           </span>{" "}
           in the `.env` file and check the setup in the
           <span className="font-bold"> source code</span> and
@@ -39,7 +39,7 @@ function App() {
         <p>
           Original proxy endpoint:{" "}
           <span className="font-bold underline">
-            {import.meta.env.VITE_URL_VERSION === "api1"
+            {import.meta.env.VITE_URL_NAME === "development"
               ? `https://jsonplaceholder.typicode.com/posts`
               : `https://dummyjson.com/posts`}
           </span>
@@ -47,7 +47,7 @@ function App() {
         <p>
           Network data endpoint:{" "}
           <span className="font-bold underline">
-            /{import.meta.env.VITE_URL_VERSION}/posts
+            http://localhost:5173/{import.meta.env.VITE_URL_NAME}/posts
           </span>
         </p>
       </div>
